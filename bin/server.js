80f08f711bd01e9ce6892efb7d8d5406f647a039
User <alecperkey@gmail.com>
2016-07-22 16:10:59 -0400

    feat(require global constants in server.js)

--- bin/server.js
+++ bin/server.js
@@ -5,10 +5,13 @@ var rootDir = path.resolve(__dirname, '..');
 /**
  * Define isomorphic constants.
  */
+global._ = require('underscore');
+global.m = require('moment');
 global.__CLIENT__ = false;
 global.__SERVER__ = true;
 global.__DISABLE_SSR__ = false;  // <----- DISABLES SERVER SIDE RENDERING FOR ERROR DEBUGGING
 global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
+
 
 if (__DEVELOPMENT__) {
   if (!require('piping')({
